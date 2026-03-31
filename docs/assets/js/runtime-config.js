d4a8663ae99bc58169acf5f45857a40f0ae4c995
STRUCTUREBEFOREWORDS <taiki.color.digital.art@gmail.com>
2026-03-31 15:26:02 +0900

    rebuild: restore Figma Make build

--- docs/assets/js/runtime-config.js
+++
@@ -1,8 +0,0 @@
-window.TCDA_RUNTIME_CONFIG = {
-  apiBase: "/api",
-  catalogEndpoint: "/api/catalog",
-  stripeCheckoutEndpoint: "/api/checkout/stripe-session",
-  printfulOrderEndpoint: "/api/fulfillment/printful-order",
-  accountEndpoint: "/api/account",
-  currency: "JPY",
-};
