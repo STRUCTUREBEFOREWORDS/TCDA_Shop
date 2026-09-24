f396d46cf895626d1fbc49cb86238628754b5234
STRUCTUREBEFOREWORDS <249972975+STRUCTUREBEFOREWORDS@users.noreply.github.com>
2026-09-24 10:24:23 +0900

    build: rebuild on Mac Studio

--- docs/assets/JsonLd-CX6vuBBq.js
+++
@@ -1,1 +0,0 @@
-import{j as s,H as i}from"./index-hMzFgJ3S.js";function r({data:t}){return s.jsx(i,{children:s.jsx("script",{type:"application/ld+json",children:JSON.stringify(t)})})}export{r as J};
